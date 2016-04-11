effd971a6fb24f801536af13c171c167ea068bb9
kanonieer <kanonieer@o2.pl>
2016-04-11 12:24:50 +0200

    first upload

---
+++ Puzzle/Assets/Scripts/OnMouseClick.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnMouseClick : MonoBehaviour {
+
+    public float smooth = 1f;
+
+    private Vector3 targetAngles;
+
+	void OnMouseDown()
+    {
+       
+        if (gameObject.tag == "false")
+        {
+            if (GameObject.FindGameObjectsWithTag("true").Length == 2) { }
+            else{
+            gameObject.transform.tag = "true";
+             }
+       }
+    }
+    void Update()
+    {
+        
+        if (gameObject.transform.tag == "true")
+        {
+            if(gameObject.transform.eulerAngles.y<180)
+            {
+                // some condition to rotate 180
+                targetAngles = transform.eulerAngles + 80f * Vector3.up; // what the new angles should be
+                                                                          // transform.Rotate(0, Time.deltaTime * 30, 0, Space.Self);
+                transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngles, 4*smooth * Time.deltaTime); // lerp to new angles
+            }
+        }
+      
+    }
+  
+}
