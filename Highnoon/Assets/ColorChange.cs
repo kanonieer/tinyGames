74f1cc5f3adc7e63d5ab625e7347b1bc2493bafd
kanonieer <kanonieer@o2.pl>
2016-04-26 21:34:14 +0200

    highnoon

---
+++ Highnoon/Assets/ColorChange.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChange : MonoBehaviour
+{
+        Gradient g;
+        GradientColorKey[] gck;
+        GradientAlphaKey[] gak;
+    float time;
+    bool switcher;
+    void Start()
+    {
+
+        g = new Gradient();
+        gck = new GradientColorKey[2];
+        gck[0].color = Color.red;
+        gck[0].time = 0.0F;
+        gck[1].color = Color.blue;
+        gck[1].time = 1.0F;
+        gak = new GradientAlphaKey[2];
+        gak[0].alpha = 1.0F;
+        gak[0].time = 0.0F;
+        gak[1].alpha = 0.0F;
+        gak[1].time = 1.0F;
+         g.SetKeys(gck, gak);
+        time = 0.0F;
+        switcher = true;
+    }
+   
+
+     
+    void FixedUpdate()
+    {
+        if (time < 1.0F && switcher == true)
+        {
+            time =+ 0.01F;
+            if(time == 1.0F)
+            {
+                switcher = false;
+            }
+        }
+        else
+        {
+                time = -0.01F;
+            if (time == 0.0F)
+            {
+                switcher = true;
+            }
+            
+            
+        }
+        gameObject.GetComponent<Renderer>().material.color = g.Evaluate(time);
+    
+    }
+}
---
+++ Highnoon/Assets/Rotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+    public float smooth = 1f;
+
+    private Vector3 targetAngles;
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void Update()
+    {
+
+
+                targetAngles = transform.eulerAngles + 90f * Vector3.up; // what the new angles should be
+                                                                         // transform.Rotate(0, Time.deltaTime * 30, 0, Space.Self);
+                transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngles, 4 * smooth * Time.deltaTime); // lerp to new angles
+            
+        
+
+    }
+}
